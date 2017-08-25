25a266a60ffc1b8a81dca593d6a0bf40ee9fcec8
daktak <daktak@domo.ddns.net>
2017-08-25 15:30:12 +1000

    update fdroid repo

--- get.sh
+++ get.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 URL=$1
-GPLAYDIR="/var/www/locahost/htdocs/fdroid-gp/repo/"
+GPLAYDIR="/var/www/localhost/htdocs/fdroid-gp/repo/"
 OTHERDIR="/var/www/localhost/htdocs/fdroid/repo/"
 GPLAYCMD=/usr/bin/gplaycli
+FDROIDCMD=/usr/bin/fdroid
+TMPFILE=/tmp/fd-run.sh
 if [[ "$URL" =~ ^https://play.google.com/.* ]]; then
 	IFS='='
 	array=( ${URL} ) 
 	echo ${array[1]}
-	cd "${GPLAYDIR}"
-	${GPLAYCMD} -d ${array[1]}
+	echo "#!/bin/bash" > ${TMPFILE}
+	echo "cd "${GPLAYDIR}"" >> ${TMPFILE}
+	echo "${GPLAYCMD} -d ${array[1]}" >>  ${TMPFILE}
 else
 	echo $URL
-	cd "${OTHERDIR}"
-	wget "${URL}"
+	echo "cd "${OTHERDIR}"" >  ${TMPFILE}
+	echo "wget "${URL}"" >> ${TMPFILE}
 fi
-#cd ..
-#python2 /usr/bin/fdroid update -c --pretty 
+echo "cd .." >>  ${TMPFILE}
+echo "${FDROIDCMD} update -c --pretty" >>  ${TMPFILE}
+/usr/bin/screen -dmS FDROID /bin/sh  ${TMPFILE}
